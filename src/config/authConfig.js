1fb60c4d0ec2cefbab9f7e0d29b48f172ac6eae8
boot88 <116805971+boot88@users.noreply.github.com>
2026-04-15 13:22:37 +0700

    Login redesign C: finalize clean intranet chemistry theme and input fixes

--- src/config/authConfig.js
+++ src/config/authConfig.js
@@ -21,6 +21,12 @@ export const ADMIN_CREDENTIALS = [
   }
 ];
 
+export const MANAGER_CREDENTIALS = {
+  username: 'manager_nioh',
+  password: 'MngNioh!2026',
+  name: 'Старший сотрудник'
+};
+
 export const LOCAL_EMPLOYEES_KEY = 'registeredEmployees';
 export const AUTH_STATE_KEY = 'authState';
 export const CHAT_THREADS_KEY = 'employeeChatThreads';
